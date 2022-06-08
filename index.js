45b399cb4e8da743b83bba99bbb4fcc407879db2
Eric Mukuria <mukuriaeric@gmail.com>
2022-06-08 06:11:20 -0700

    commit tree 7647dad454e0294df70613f426de8f60b6260ac4

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 
 // Write your code here
+let num1= 31;
+let num2= 2;
+let multiply = num1* num2
+
+let random = Math.floor(Math.random() * 10) + 1
+
+let mod = 40 % 9;
+
+let max  = Math.max(6, 9, 12, 15, 20)
